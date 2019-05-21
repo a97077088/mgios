f2573bfbbcc9f3f6e8ec37e63b4b306bd74e3169
fbi <97077088@qq.com>
2019-05-22 00:12:40 +0800

    updatego

--- tsdebug/agent.js
+++ tsdebug/agent.js
@@ -1,7 +1,19 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
-console.log("test");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+try {
+  console.log("run main");
+} catch (e) {
+  console.log(e);
+}
+
+function main() {
+  console.log("run main");
+}
 
 },{}]},{},[1])
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkM6L1VzZXJzL2ZiaS9BcHBEYXRhL1JvYW1pbmcvbnBtL25vZGVfbW9kdWxlcy9mcmlkYS1jb21waWxlL25vZGVfbW9kdWxlcy9icm93c2VyLXBhY2svX3ByZWx1ZGUuanMiLCJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7O0FDQUEsT0FBTyxDQUFDLEdBQVIsQ0FBWSxNQUFaIiwiZmlsZSI6ImdlbmVyYXRlZC5qcyIsInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkM6L1VzZXJzL2ZiaS9BcHBEYXRhL1JvYW1pbmcvbnBtL25vZGVfbW9kdWxlcy9mcmlkYS1jb21waWxlL25vZGVfbW9kdWxlcy9icm93c2VyLXBhY2svX3ByZWx1ZGUuanMiLCJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7Ozs7OztBQ0NBLElBQUc7QUFDQyxFQUFBLE9BQU8sQ0FBQyxHQUFSLENBQVksVUFBWjtBQUNILENBRkQsQ0FFQyxPQUFNLENBQU4sRUFBUTtBQUNMLEVBQUEsT0FBTyxDQUFDLEdBQVIsQ0FBWSxDQUFaO0FBQ0g7O0FBRUQsU0FBUyxJQUFULEdBQWE7QUFDVCxFQUFBLE9BQU8sQ0FBQyxHQUFSLENBQVksVUFBWjtBQUlIIiwiZmlsZSI6ImdlbmVyYXRlZC5qcyIsInNvdXJjZVJvb3QiOiIifQ==
--- tsdebug/index.ts
+++ tsdebug/index.ts
@@ -1,1 +1,13 @@
-console.log("test")
+import *as ios from "frida-lib/ios"
+try{
+    console.log("run main")
+}catch(e){
+    console.log(e)
+}
+
+function main(){
+    console.log("run main")
+
+
+
+}
